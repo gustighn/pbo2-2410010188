691d776477da8e42184ec633de5fe1461a3f20ca
gustighn <gustigani903@gmail.com>
2026-09-19 23:52:31 +0800

    Menambah class KartuMahasiswa

---
+++ p01-halo-java/src/main/java/id/ac/uniska/pbo2/p01/KartuMahasiswa.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package id.ac.uniska.pbo2.p01;
+
+/**
+ *
+ * @author ASUS
+ */
+public class KartuMahasiswa {
+    
+    public static void main(String[] args) {
+        String nama = "Gusti Muhammad Abdul Ghani";
+        String npm = "2410010188";
+        String prodi = "Teknik Informatika";
+        int semester = 5;
+
+        System.out.println("======================================================");
+        System.out.println("      KARTU MAHASISWA PBO 2");
+        System.out.println("======================================================");
+        System.out.println("Nama      : " + nama);
+        System.out.println("NPM       : " + npm);
+        System.out.println("Prodi     : " + prodi);
+        System.out.println("Semester  : " + semester);
+        System.out.println("Alasan    : Ingin membuat aplikasi pencatatan keuangan");
+        System.out.println("======================================================");
+    }
+    
+}
